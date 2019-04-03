3716a65ca6a464c01a4372e39690e33224791852
Fagner <meurerfagner@hotmail.com>
2019-04-03 00:38:16 -0300

    Criado o Drawer Menu

---
+++ app/src/main/java/com/example/exmenudrawer/Tela1.kt
@@ -0,0 +1,12 @@
+package com.example.exmenudrawer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Tela1 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela1)
+    }
+}
